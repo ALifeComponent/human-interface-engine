a0126277dc48068357e4f9c5f86a019a0a0669ea
Satsuki Akiba <anosatsuk124@gmail.com>
2025-05-14 08:59:03 +0900

    WIP

--- components/viewer/build.rs
+++ components/viewer/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     tonic_build::configure()
-        .build_server(false)
+        .build_server(true)
         .compile_protos(
             &["../../proto/viewer/v1/viewer.proto"],
             // The path to search for includes
--- components/viewer/src/rpc/serve.rs
+++ components/viewer/src/rpc/serve.rs
@@ -1,33 +1,47 @@
-use super::proto::generated::SayHelloRequest;
-use super::proto::generated::greeter_service_client::GreeterServiceClient;
+use super::proto::generated::greeter_service_server::{GreeterService, GreeterServiceServer};
+use super::proto::generated::{SayHelloRequest, SayHelloResponse};
+
 use bevy::prelude::*;
 
-use std::sync::mpsc::{Receiver, channel};
+use tonic::{Request, Response, Status, transport::Server};
 
-#[derive(Resource)]
-struct HelloReceiver(Receiver<String>);
+#[derive(Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl GreeterService for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<SayHelloRequest>,
+    ) -> Result<Response<SayHelloResponse>, Status> {
+        let name = request.into_inner().name;
+        let reply = SayHelloResponse {
+            message: format!("こんにちは、{}さん！", name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+pub async fn serve_grpc(addr: std::net::SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+    let greeter = MyGreeter::default();
+    Server::builder()
+        .add_service(GreeterServiceServer::new(greeter))
+        .serve(addr)
+        .await?;
+    Ok(())
+}
 
 pub fn spawn_grpc_request_system(mut commands: Commands) {
-    let (tx, rx) = channel();
-    std::thread::spawn(move || {
-        let rt = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .expect("failed to build Tokio runtime");
-        let message = rt.block_on(async {
-            let mut client = GreeterServiceClient::connect("http://[::1]:50051")
+    // Bevy の起動直後に gRPC サーバを別スレッドで稼働させる
+    let addr = self.addr;
+    thread::spawn(move || {
+        // 独自 Tokio ランタイムを使って非同期ブロックを実行
+        let rt = Runtime::new().expect("Failed to create Tokio runtime");
+        rt.block_on(async {
+            grpc::serve_grpc(addr)
                 .await
-                .expect("connect failed");
-            let resp = client
-                .say_hello(SayHelloRequest {
-                    name: "World".into(),
-                })
-                .await
-                .expect("rpc failed")
-                .into_inner();
-            resp.message
+                .expect("gRPC サーバの起動に失敗しました");
         });
-        tx.send(message).expect("send failed");
     });
-    commands.insert_resource(HelloReceiver(rx));
+    info!("gRPC server thread spawned on {}", self.addr);
 }
