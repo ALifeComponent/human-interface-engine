b6d09989731084ba807d4bc77c6cbe6cee336fb7
Satsuki Akiba <anosatsuk124@gmail.com>
2025-05-24 02:25:05 +0900

    Add `SetObjectPosition` benchmark

--- examples/benchmark_01/main.go
+++ examples/benchmark_01/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 	"math/rand/v2"
-	"time"
 
 	viewer "github.com/ALifeComponent/human-interface-engine/gen/go/viewer/v1"
 	"google.golang.org/grpc"
@@ -20,60 +19,104 @@ func main() {
 
 	client := viewer.NewManageObjectServiceClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	makeRandomRequest := func() *viewer.SpawnObjectRequest {
-		randomPosition := &viewer.Vector3{
-			X: rand.Float32() * 100,
-			Y: rand.Float32() * 100,
-			Z: rand.Float32() * 100,
-		}
-
-		randomSize := rand.Float32() * 10
-
-		randomColor := &viewer.RGBA{
-			R: rand.Float32(),
-			G: rand.Float32(),
-			B: rand.Float32(),
-			A: 1.0,
-		}
-
-		randomShape := rand.UintN(2) + 1
-
-		return &viewer.SpawnObjectRequest{
-			ObjectProperties: &viewer.ObjectProperties{
-				Shape: viewer.ObjectShape(randomShape),
-				Size: &viewer.ObjectSize{
-					Value: randomSize,
-				},
-				Color: &viewer.ObjectColor{
-					Color: &viewer.ObjectColor_ColorRgba{
-						ColorRgba: randomColor,
-					},
-				},
-			},
-			Position: randomPosition,
-		}
-	}
+	ctx := context.Background()
 
 	// 100 sequence messages filled with random data
 	var reqs *viewer.SpawnObjectSequenceRequest = &viewer.SpawnObjectSequenceRequest{
 		Requests: make([]*viewer.SpawnObjectRequest, 100),
 	}
 	for i := range 100 {
-		reqs.Requests[i] = makeRandomRequest()
+		reqs.Requests[i] = makeRandomSpawnObjectRequest()
 	}
 
+	var uuids [][]byte = make([][]byte, 100*100)
+
 	// Send 100 requests of SpawnObjectSequenceRequest thath contains 100 SpawnObjectRequest (Spawing 10000 objects)
-	for range 100 {
+	for i := range 100 {
 		resp, err := client.SpawnObjectSequence(ctx, reqs)
 		if err != nil {
-			log.Fatalf("Echo RPC failed: %v", err)
+			log.Fatalf("RPC failed: %v", err)
 		}
 
-		for _, r := range resp.Responses {
+		for j, r := range resp.Responses {
 			log.Printf("Response: %v", r)
+			if r.SpawendObjectId.Uuid != nil {
+				uuids[j+i] = r.SpawendObjectId.Uuid.Value
+			}
 		}
+	}
+
+	for range 1000 {
+
+		// Send 100*100 requests of `SetObjectPositionRequest` that contains 100 `SetObjectPositionRequest` (Moving 10000 objects)
+		for i := range 100 {
+			var reqs2 *viewer.SetObjectPositionSequenceRequest = &viewer.SetObjectPositionSequenceRequest{
+				Requests: make([]*viewer.SetObjectPositionRequest, 100),
+			}
+			for j := range 100 {
+				reqs2.Requests[j] = makeRandomSetObjectPositionRequest(uuids[j+i])
+			}
+
+			resp2, err := client.SetObjectPositionSequence(ctx, reqs2)
+			if err != nil {
+				log.Fatalf("RPC failed: %v", err)
+			}
+
+			for _, r := range resp2.Responses {
+				log.Printf("Response: %v", r)
+			}
+
+		}
+	}
+}
+
+func makeRandomSetObjectPositionRequest(uuid []byte) *viewer.SetObjectPositionRequest {
+	randomPosition := &viewer.Vector3{
+		X: rand.Float32() * 100,
+		Y: rand.Float32() * 100,
+		Z: rand.Float32() * 100,
+	}
+
+	return &viewer.SetObjectPositionRequest{
+		ObjectId: &viewer.ObjectId{
+			Uuid: &viewer.Uuid{
+				Value: uuid,
+			},
+		},
+		Position: randomPosition,
+	}
+}
+
+func makeRandomSpawnObjectRequest() *viewer.SpawnObjectRequest {
+	randomPosition := &viewer.Vector3{
+		X: rand.Float32() * 100,
+		Y: rand.Float32() * 100,
+		Z: rand.Float32() * 100,
+	}
+
+	randomSize := rand.Float32() * 10
+
+	randomColor := &viewer.RGBA{
+		R: rand.Float32(),
+		G: rand.Float32(),
+		B: rand.Float32(),
+		A: 1.0,
+	}
+
+	randomShape := rand.UintN(2) + 1
+
+	return &viewer.SpawnObjectRequest{
+		ObjectProperties: &viewer.ObjectProperties{
+			Shape: viewer.ObjectShape(randomShape),
+			Size: &viewer.ObjectSize{
+				Value: randomSize,
+			},
+			Color: &viewer.ObjectColor{
+				Color: &viewer.ObjectColor_ColorRgba{
+					ColorRgba: randomColor,
+				},
+			},
+		},
+		Position: randomPosition,
 	}
 }
